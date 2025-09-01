4669f89fab844b3634c8163dc0785649fe1d6562
zerixmax <zerix.m@gmail.com>
2025-09-01 20:17:35 +0200

    Create varpyz.py
    prvo pisanje koda

---
+++ varpyz.py
@@ -0,0 +1,11 @@
+ # varijale vjezba
+ime = "Ivo"
+prezime = "Cetinić"
+godina_rodenja = "1976"
+drzava_rodenja = " Hrvatska
+ststus_radnog_odnosa = "nezaposlen"
+tezina = 68.5 #u kilogramima
+spol = "musko"
+# Varijable za stranice četverokutai površinu
+a = 10
+b =5

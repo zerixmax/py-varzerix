f1c261020c877d7c179fb5f8d0af6fd8859260ae
zerixmax <zerix.m@gmail.com>
2025-09-01 21:39:03 +0200

    Update varpyz.py

--- varpyz.py
+++ varpyz.py
@@ -1,12 +1,23 @@
- # varijale vjezba
 ime = "Ivo"
 prezime = "Cetinić"
 godina_rodenja = "1976"
-drzava_rodenja = " Hrvatska
-ststus_radnog_odnosa = "nezaposlen"
-tezina = 68.5 #u kilogramima
+drzava_rodenja = "Hrvatska"
+status_radnog_odnosa = "nezaposlen"
+tezina = 68.5 # u kilogramima
 spol = "musko"
 # Varijable za stranice četverokutai površinu
 a = 10
 b = 5
-print(a * b)
+povrsina = a * b
+
+print(povrsina)     
+print("Ime:", ime)
+print("Prezime:", prezime)
+print("Godina rođenja:", godina_rodenja)
+print("Država rođenja:", drzava_rodenja)
+print("Status radnog odnosa:", status_radnog_odnosa)
+print("Težina:", tezina)
+print("Spol:", spol)
+print("Stranica a:", a)
+print("Stranica b:", b)
+print("Površina četverokuta:", povrsina)

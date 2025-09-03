c8d16eda6ae1e73065f75a97325e88dc90c21ee1
zerixmax <zerix.m@gmail.com>
2025-09-03 19:04:45 +0200

    Update new2.py

--- new2.py
+++ new2.py
@@ -10,28 +10,33 @@ c = 13
 pohranjeni podaci o filmu: 
 naziv filma, godina proizvodnje, proječna ocjena, kratki opis •"""
 
-naziv_filma = "Bladerunner"
-godina_proizvodnje = 1982
-projecna_ocjena = 8.8
-kratki_opis = "Dali Android sanja električne ovce?" 
+movie_name = "Bladerunner"
+release_date = 1982
+average_score = 8.8
+short_description = "Dali Android sanja električne ovce?" 
 
 """Potrebno je kreirati variajble u kojima će biti pohranjeni 
 podaci o
 imenu i prezimenu studenta, 
 završnoj ocjeni te podataka je li
 diplomirao ili nije"""
-ime_studenta = "Ivo"
-prezime_studenta = "Cetinić"
-zavrsna_ocjena = 5
-diplomirao = True   
+
+student_name = "Ivo"
+student_surname = "Cetinić"
+final_grade = 5
+graduated = True
+
 """Kreirati varijablu za čuvanje podataka o studentu: ime i prezime,
 OIB, email, broj telefona, prosječna ocjena, godina upisa
 studija, naziv studija, naziv sveučilišta"""    
+
 OIB = "12345678901"
 email = "ivo@korcula.me"
-broj_telefona = "+38512345678"
-prosjecna_ocjena = 4.5
-godina_upisa = 1999
-naziv_studija = "Brodostrojarstvo"
-naziv_sveucilista = "Sveučilište u Dubrovniku " 
+mobilephone_number = "+38512345678"
+average_grade = 4.5
+enrollment_year = 1999
+study_name = "Brodostrojarstvo"
+university_name = "Sveučilište u Dubrovniku "
+
+
 

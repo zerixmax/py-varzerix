0c1d583f165604757d372998a256e7a3c7d40792
zerixmax <zerix.m@gmail.com>
2025-09-03 19:00:12 +0200

    Update new2.py

--- new2.py
+++ new2.py
@@ -1,1 +1,37 @@
-print("ALo Svite evo me ")
+# komentar
+"""Potrebno je kreirati variajble 
+u kojima će biti pohranjene proizvoljne dužine trokuta."""
+
+a = 5
+b = 8
+c = 13
+
+""" Potrebno je kreirati variajble u kojima će biti 
+pohranjeni podaci o filmu: 
+naziv filma, godina proizvodnje, proječna ocjena, kratki opis •"""
+
+naziv_filma = "Bladerunner"
+godina_proizvodnje = 1982
+projecna_ocjena = 8.8
+kratki_opis = "Dali Android sanja električne ovce?" 
+
+"""Potrebno je kreirati variajble u kojima će biti pohranjeni 
+podaci o
+imenu i prezimenu studenta, 
+završnoj ocjeni te podataka je li
+diplomirao ili nije"""
+ime_studenta = "Ivo"
+prezime_studenta = "Cetinić"
+zavrsna_ocjena = 5
+diplomirao = True   
+"""Kreirati varijablu za čuvanje podataka o studentu: ime i prezime,
+OIB, email, broj telefona, prosječna ocjena, godina upisa
+studija, naziv studija, naziv sveučilišta"""    
+OIB = "12345678901"
+email = "ivo@korcula.me"
+broj_telefona = "+38512345678"
+prosjecna_ocjena = 4.5
+godina_upisa = 1999
+naziv_studija = "Brodostrojarstvo"
+naziv_sveucilista = "Sveučilište u Dubrovniku " 
+
